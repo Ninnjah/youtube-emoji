79776cf6e721b82affe6c89b8d5b6e8326def730
ninnjah <yand.man4@gmail.com>
2022-02-05 03:19:05 +0300

    Add folder for images

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import requests
 
@@ -10,13 +11,16 @@ logger = logging.getLogger(__name__)
 
 emoji_url: str = "https://www.youtube.com/s/gaming/emoji/7ff574f2/emoji_u{:04d}.png"
 
+if not os.path.exists("emoji/"):
+    os.mkdir("emoji")
+
 logger.info("Download start")
 for i in range(1, 10000):
     url: str = emoji_url.format(i)
 
     r = requests.get(url)
     if r.status_code == 200:
-        with open(f"u{i:04d}.png", "wb") as image:
+        with open(f"emoji/u{i:04d}.png", "wb") as image:
             image.write(r.content)
 
         logger.info(f"Emoji #{i:04d} downloaded! {9999 - i} left")
