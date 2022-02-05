c5cb3e45f636edbeed75c49990a4ed01c47aed2f
ninnjah <yand.man4@gmail.com>
2022-02-05 03:17:19 +0300

    Update logging format Add start message

--- __main__.py
+++ __main__.py
@@ -1,10 +1,16 @@
 import logging
 import requests
 
+logging.basicConfig(
+    format="[%(asctime)s] \"%(name)s\" | %(levelname)s: %(message)s",
+    datefmt="%y.%m.%d %H:%M:%S",
+    level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
 emoji_url: str = "https://www.youtube.com/s/gaming/emoji/7ff574f2/emoji_u{:04d}.png"
 
+logger.info("Download start")
 for i in range(1, 10000):
     url: str = emoji_url.format(i)
 
