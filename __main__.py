a3d58c54ec3eaf1c0198b0a2672e95bbdbfa55a4
ninnjah <yand.man4@gmail.com>
2022-02-05 21:23:19 +0300

    Add hex numbers support

--- __main__.py
+++ __main__.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Emoji url template
-EMOJI_URL: str = "https://www.youtube.com/s/gaming/emoji/7ff574f2/emoji_u{:04d}.png"
+EMOJI_URL: str = "https://www.youtube.com/s/gaming/emoji/7ff574f2/emoji_u{}.png"
 
 # Checking for "emoji" folder
 if not os.path.exists("emoji/"):
@@ -19,23 +19,25 @@ if not os.path.exists("emoji/"):
 
 # Start downloading emoji images
 logger.info("Download start")
-for i in range(1, 10000):
+for i in range(0x0, 0x1ffff + 1):
+    # Generate hex number
+    num: str = hex(i).replace("0x", "").rjust(4, "0")
     # Generate emoji url
-    url: str = EMOJI_URL.format(i)
+    url: str = EMOJI_URL.format(num)
 
     # Request emoji
     r = requests.get(url)
 
     # If emoji exists download it
     if r.status_code == 200:
-        with open(f"emoji/u{i:04d}.png", "wb") as image:
+        with open(f"emoji/u{num}.png", "wb") as image:
             image.write(r.content)
 
         # Report about downloaded emoji
-        logger.info(f"Emoji #{i:04d} downloaded! {9999 - i} left")
+        logger.info(f"Emoji #{i} downloaded! {0x1ffff - i} left")
 
     elif r.status_code == 404:
-        logger.warning(f"Emoji #{i:04d} not exists!  {9999 - i} left")
+        logger.warning(f"Emoji #{i} not exists!  {0x1ffff - i} left")
 
     else:
-        logger.error(f"Emoji #{i:04d} status code: {r.status_code}")
+        logger.error(f"Emoji #{i} status code: {r.status_code}")
