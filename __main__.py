344e95dcacd6bdbd950762d1393c702881a60036
ninnjah <yand.man4@gmail.com>
2022-02-05 03:23:47 +0300

    Add comments

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import os
 import logging
 import requests
 
+# Config logger
 logging.basicConfig(
     format="[%(asctime)s] \"%(name)s\" | %(levelname)s: %(message)s",
     datefmt="%y.%m.%d %H:%M:%S",
@@ -9,18 +10,26 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-emoji_url: str = "https://www.youtube.com/s/gaming/emoji/7ff574f2/emoji_u{:04d}.png"
+# Emoji url template
+EMOJI_URL: str = "https://www.youtube.com/s/gaming/emoji/7ff574f2/emoji_u{:04d}.png"
 
+# Checking for "emoji" folder
 if not os.path.exists("emoji/"):
     os.mkdir("emoji")
 
+# Start downloading emoji images
 logger.info("Download start")
 for i in range(1, 10000):
-    url: str = emoji_url.format(i)
+    # Generate emoji url
+    url: str = EMOJI_URL.format(i)
 
+    # Request emoji
     r = requests.get(url)
+
+    # If emoji exists download it
     if r.status_code == 200:
         with open(f"emoji/u{i:04d}.png", "wb") as image:
             image.write(r.content)
 
+        # Report about downloaded emoji
         logger.info(f"Emoji #{i:04d} downloaded! {9999 - i} left")
